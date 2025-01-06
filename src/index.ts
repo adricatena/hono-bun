5875df1883b99ff6cde1f01b09228cd74e8b6308
Adriano <adricatena@gmail.com>
2025-01-06 01:59:21 -0300

    Update .gitignore and add VSCode settings; refactor imports and improve code formatting in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors';
-import { csrf } from 'hono/csrf';
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
 
 type Bindings = {
-  CORS_ORIGIN: string
-}
+  CORS_ORIGIN: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>().basePath('/api');
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
 
-app.use("*", csrf())
+app.use("*", csrf());
 
 app.use("*", async (c, next) => {
   const corsMiddlewareHandler = cors({
     origin: c.env.CORS_ORIGIN,
-  })
-  return corsMiddlewareHandler(c, next)
-})
+  });
+  return corsMiddlewareHandler(c, next);
+});
 
-app.get('/hola-mundo', (c) => {
-  return c.json({ message: 'Hello Hono!' })
-})
+app.get("/hola-mundo", (c) => {
+  return c.json({ message: "Hello Hono! con CICD" });
+});
 
-export default app
+export default app;
